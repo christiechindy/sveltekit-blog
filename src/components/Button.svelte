99580b05c667da26379dc599bf6a1a19b9a29104
christiechindy <chindychristie@gmail.com>
2024-07-06 18:00:40 +0800

    pass function to button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
     export let text:string;
+    
+    const dispatch = createEventDispatcher();
+    const dispatchFunc = () => {
+        dispatch("click", {
+            text: "Helo"
+        })
+    }
 </script>
 
-<button class="text-center capitalize w-full bg-my-purple rounded-xl py-2 text-white hover:bg-my-dark_green transition duration-500 ease-in-out">{text}</button>
+<button on:click={dispatchFunc} class="text-center capitalize w-full bg-my-purple rounded-xl py-2 text-white hover:bg-my-dark_green transition duration-500 ease-in-out">{text}</button>
