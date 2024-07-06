9bb818e602adf9c8a71ab852c604acd48ed1866f
christiechindy <chindychristie@gmail.com>
2024-07-06 18:05:46 +0800

    static done

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -3,20 +3,20 @@
     import AuthTitle from "../../components/AuthTitle.svelte";
     import Button from "../../components/Button.svelte";
 
-    const handleLogin = async () => {
-        console.log("loginx!!");
+    const handleSignup = () => {
+
     }
 </script>
-
 <div class="page flex justify-center align-middle h-screen">
     <div class="w-fit wrapper flex flex-col align-middle justify-center">
-        <AuthTitle title="Login" />
+        <AuthTitle title="Register" />
 
+        <AuthInput label="full name" />
         <AuthInput label="username" />
         <AuthInput label="password" />
 
-        <Button text="login" on:click={handleLogin} />
+        <Button text="Sign Up" on:click={handleSignup} />
 
-        <div class="text-sm text-my-text_gray mt-8">Don't have an account? <a class="text-my-purple font-medium hover:text-my-blue transition duration-300 ease-in-out" href="/sign-up">Register!</a></div>
+        <div class="text-sm text-my-text_gray mt-8">Already have an account? <a class="text-my-purple font-medium hover:text-my-blue transition duration-300 ease-in-out" href="/login">Login!</a></div>
     </div>
 </div>
