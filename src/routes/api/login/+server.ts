f4c538d8f18ffd914bdc21300be6b459e814a50c
christiechindy <chindychristie@gmail.com>
2024-07-08 03:00:47 +0800

    login api

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -1,41 +1,48 @@
 import { json } from "@sveltejs/kit";
 import UserSchema from "../../../models/UserSchema"
 import { failMeta, successMeta } from "$lib/meta";
-
-export const GET = async () => {
-    try {
-        const users = await UserSchema.find();
-
-        return json({
-            meta: successMeta,
-            data: {
-                users
-            }
-        });
-    } catch (err) {
-        return json({
-            meta: failMeta,
-            data: err
-        })
-    }
-}
+import * as bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 export const POST = async ({ request }) => {
     try {
         const {username, password} = await request.json();
         
-        const user = new UserSchema({
-            username, password
-        });
-    
-        await user.save();
+        const findUser = await UserSchema.find({"username": username});
+        
+        if (findUser.length === 0) {
+            return json({
+                meta: {
+                    code: 404,
+                    status: false,
+                    message: "User not found!"
+                }
+            })
+        } else {
+            const isPasswordCorrect = bcrypt.compareSync(password, findUser[0].password!);
 
-        return json({
-            meta: successMeta,
-            data: {
-                user
+            if (!isPasswordCorrect) {
+                return json({
+                    meta: {
+                        code: 400,
+                        status: false,
+                        message: "The password doesn't match"
+                    }
+                })
+            } else {
+                const token = jwt.sign({
+                    username: findUser[0].username
+                }, "jdf8g3");
+                const headers = new Headers();
+                headers.append('set-cookie', `accessToken=${token}; HttpOnly; Path=/; Max-Age=3600`);
+                return json({
+                    meta: successMeta,
+                    data: {
+                        fullName: findUser[0].fullName
+                    }
+                }, {headers})
             }
-        });
+        }
     } catch (err) {
         return json({
             meta: failMeta,
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,22 +1,83 @@
-<script>
+<script lang="ts">
     import AuthInput from "../../components/AuthInput.svelte";
     import AuthTitle from "../../components/AuthTitle.svelte";
     import Button from "../../components/Button.svelte";
+    import { z } from "zod";
+    import axios from "axios";
+    import type { TApiResp } from "../../types/ApiTypes";
+    import { goto } from '$app/navigation';
 
+    interface ILogin {
+        username: string,
+        password: string,
+    }
+
+    let hasFocused = {
+        username: false,
+        password: false
+    }
+
+    let loginData:ILogin = {
+        username: "",
+        password: ""
+    }
+
+    const loginSchema = z.object({
+        username: z.string().min(2, { message: "The username is invalid!" }),
+        password: z.string().min(8, { message: "Password must be at least 8 characters" })
+    })
+
+    let errors:ILogin = {
+        username: "",
+        password: ""
+    }
+    let isFormValid = false;
+
+    $: {
+        try {
+            errors = {username: "", password: ""};
+            loginSchema.parse(loginData);
+            isFormValid = true;
+        } catch (err) {
+            isFormValid = false;
+            if (err instanceof z.ZodError) {
+                err.errors.forEach((error) => {
+                    errors[error.path[0] as keyof ILogin] = error.message;
+                });
+            }
+        }
+    }
+
+    let isLoading = false;
+    let errorLogin = "";
     const handleLogin = async () => {
-        console.log("loginx!!");
+        if (isFormValid) {
+            isLoading = true;
+            const ax = await axios.post("/api/login", loginData);
+            const res:TApiResp = ax?.data;
+            isLoading = false;
+            if (res.meta.status) {
+                goto("/home");
+            } else {
+                errorLogin = res.meta.message;
+            }
+        }
     }
 </script>
 
-<div class="page flex justify-center align-middle h-screen">
-    <div class="w-fit wrapper flex flex-col align-middle justify-center">
+<div class="page flex justify-center items-center h-screen">
+    <div class="w-fit wrapper flex flex-col items-center justify-center gap-7">
         <AuthTitle title="Login" />
 
-        <AuthInput label="username" />
-        <AuthInput label="password" />
+        <div>
+            <AuthInput label="username" bind:value={loginData.username} hasFocused={hasFocused.username} bind:error={errors.username}  />
+            <AuthInput label="password" bind:value={loginData.password} hasFocused={hasFocused.password} bind:error={errors.password} type="password" />
+        </div>
 
-        <Button text="login" on:click={handleLogin} />
+        <Button text="login" on:click={handleLogin} disabled={!isFormValid} isLoading={isLoading} />
 
-        <div class="text-sm text-my-text_gray mt-8">Don't have an account? <a class="text-my-purple font-medium hover:text-my-blue transition duration-300 ease-in-out" href="/sign-up">Register!</a></div>
+        <div class="text-sm text-red-600 max-w-96">{errorLogin}</div>
+
+        <div class="text-sm text-my-text_gray mt-8 self-start">Don't have an account? <a class="text-my-purple font-medium hover:text-my-blue transition duration-300 ease-in-out" href="/sign-up">Register!</a></div>
     </div>
 </div>
