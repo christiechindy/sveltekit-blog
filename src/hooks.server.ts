24ffc1fb37c203a59176c5e089017884b5d1b0ca
christiechindy <chindychristie@gmail.com>
2024-07-08 03:09:04 +0800

    add middleware

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,4 +1,25 @@
 import { connectDb } from "$lib/db";
+import { redirect, type Handle } from "@sveltejs/kit";
 
 console.log("connecting to mongodb...");
 await connectDb();
+
+export const handle:Handle = async({event,resolve}) => {
+    const {cookies, url} = event;
+    const accessToken = cookies.get("accessToken");
+    const urlPath = url.pathname;
+
+    if (!accessToken && urlPath==="/login")  {
+        return resolve(event);
+    }
+
+    if (!accessToken) {
+        throw redirect(302, "/login");
+    }
+
+    if (accessToken && (urlPath==="/login" || urlPath==="/sign-up")) {
+        throw redirect(302, "/home");
+    }
+
+    return resolve(event);
+}
