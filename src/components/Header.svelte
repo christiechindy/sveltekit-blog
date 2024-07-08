9e7759fbda07a41c7e831213c246da2b45e9a4e5
christiechindy <chindychristie@gmail.com>
2024-07-08 11:09:05 +0800

    logout

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,11 @@
 <script>
+  import { goto } from "$app/navigation";
     import AccountIcon from "../icons/AccountIcon.svelte";
     import PencilIcon from "../icons/PencilIcon.svelte";
+
+    const handleLogout = () => {
+        goto("/login");
+    }
 </script>
 
 <div class="header w-full border-b-1.5 border-my-line_gray px-12 py-6 flex justify-between">
@@ -8,10 +13,10 @@
     <div class="flex gap-12">
         <a href="/write" class="flex items-center gap-4">
             <span class="text-my-text_gray">Write</span>
-            <PencilIcon />
+            <PencilIcon link="/write" />
         </a>
         <div class="flex items-center gap-4">
-            <span class="text-my-text_gray">Chindy</span>
+            <button class="text-my-text_gray" on:click={handleLogout}>Logout</button>
             <AccountIcon />
         </div>
     </div>
