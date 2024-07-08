a50b2bd27d777994c4b91b720978ab6eac21812e
christiechindy <chindychristie@gmail.com>
2024-07-08 08:21:09 +0800

    add bind value to textarea *forgot

--- src/components/BlogInput.svelte
+++ src/components/BlogInput.svelte
@@ -10,6 +10,6 @@
     {#if type==="text"}
         <input class="w-full border-1.5 border-my-line_gray rounded-xl box-border py-2 px-4 mb-1 focus:outline-1 focus:outline-my-line_d_gray focus:border-my-line_d_gray" type="text" id={label} bind:value={value}>
     {:else if type==="textarea"}
-        <textarea class="w-full border-1.5 border-my-line_gray rounded-xl box-border py-2 px-4 mb-1 focus:outline-1 focus:outline-my-line_d_gray focus:border-my-line_d_gray" rows="12" id={"ar"+label}></textarea>
+        <textarea class="w-full border-1.5 border-my-line_gray rounded-xl box-border py-2 px-4 mb-1 focus:outline-1 focus:outline-my-line_d_gray focus:border-my-line_d_gray" rows="12" id={"ar"+label} bind:value={value}></textarea>
     {/if}
 </div>
